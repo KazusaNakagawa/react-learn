67ef71e48ebe2d6f9308b0ed78a87374d00c7fea
KazusaNakagawa <kazusanakgawa@gmail.com>
2020-08-20 07:34:32 +0900

    creat class componemt and func component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+// function App() {
+//   const react = 'React lear'
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <h1>{react}</h1>
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </header>
+//     </div>
+//   );
+// }
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>React learn</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      < Cat/>
+      < Cat/>
+      < Cat/>
+      < Cat/>
     </div>
-  );
+  )
+}
+
+const Cat = () => {
+  return <div>Meow!</div>
 }
 
 export default App;
