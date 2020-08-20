75c53375881b31147d8a387722e1f862b82f3158
KazusaNakagawa <kazusanakgawa@gmail.com>
2020-08-20 21:05:34 +0900

    implement action and action creator 仕組み

---
+++ src/actions/index.js
@@ -0,0 +1,12 @@
+// viewの投影するための仕組み??
+export const INCREMENT = 'INCREMENT'
+export const DECREMENT = 'DECREMENT'
+
+export const increment = () => ({
+	type: INCREMENT
+})
+
+export const decrement = () => ({
+	type: DECREMENT
+
+})
