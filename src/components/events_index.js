cdbfa87866127c6dc6f72f4ac6011e28747502bf
KazusaNakagawa <kazusanakgawa@gmail.com>
2020-08-22 16:47:26 +0900

    implement submit,cancle buttom

--- src/components/events_index.js
+++ src/components/events_index.js
@@ -37,7 +37,6 @@ class EventsIndex extends Component {
              {this.renderEvents()}
            </tbody>
         </table>
-
         <Link to="/events/new">New Event</Link>
      </React.Fragment>
     )
--- src/components/events_new.js
+++ src/components/events_new.js
@@ -1,26 +1,40 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom'
 
-// implement func
-import { postEvents } from '../actions';
-
+// import { postEvents } from '../actions';
 
 
 class EventsNew extends Component {
-  
+
+  renderField(field) {
+    const { input, label, type, meta: { touched, error } } = field
+
+    return (<div></div>)
+  }
+
   render() {
-    const insert = "insert new"
     return (
-      <React.Fragment>
-        <div>
-          <h2>{ insert }</h2>
-        </div>
-      </React.Fragment>
+      <form>
+        <div><Field label="Title" name="title" type="text" component={this.renderField} /></div>
+        <div><Field label="Body" name="body" type="text" component={this.renderField} /></div>
+
+        <input type="submit" value="Submit" disabled={false} />
+        <Link to="/" >Cancel</Link>
+      </form>
     )
   }
 }
 
+const validate = values => {
+  const errors = {}
+
+  return errors
+}
+
 // const mapStateToProps = state => ({ postEvents })
 
-export default connect(null, null)(EventsNew)
+export default connect(null, null)(
+  reduxForm({ validate, form: 'eventNewForm'})(EventsNew)
+)
