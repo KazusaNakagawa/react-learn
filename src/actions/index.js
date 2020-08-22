c726a2d3c2e4819978dc6c06578bc48740591618
KazusaNakagawa <kazusanakgawa@gmail.com>
2020-08-22 12:53:52 +0900

    check response.data

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,11 +8,7 @@ const QUERYSTRING = '?token=token123'
 // ref: https://github.com/reduxjs/redux-thunk
 // 関数を返すことができる
 export const readEvents = () => async despatch => {	
-  const response = await axios.get(`${ROOT_URL}/events${QUERYSTRING}`)
-  console.log(response.config)
-  console.log(response)
-  console.log(response.data[0]) 
-  console.log(response.data[0]['body'], "id: " +response.data[0]['id']) 
-  despatch({type: READ_EVENTS})
+  const response = await axios.get(`${ROOT_URL}/events${QUERYSTRING}`) 
+  despatch({type: READ_EVENTS, response})
 }
 
--- src/reducers/events.js
+++ src/reducers/events.js
@@ -3,6 +3,7 @@ import { READ_EVENTS } from '../actions'
 export default (state = {}, action) => {
   switch (action.type) {
 	case READ_EVENTS:
+	  console.log(action.response.data)
       return state
 	default:
 	  return state
