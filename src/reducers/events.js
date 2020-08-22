484b939ed1fa1bf1d6c1f66697978a12b87c058c
KazusaNakagawa <kazusanakgawa@gmail.com>
2020-08-22 13:51:15 +0900

    Display event list on screen

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,20 +1,12 @@
 import _ from 'lodash'
 import { READ_EVENTS } from '../actions'
 
-export default (state = {}, action) => {
+export default (evens = {}, action) => {
   switch (action.type) {
 	case READ_EVENTS:
-	  // 取り出したい形にkeyのobject
-	  /* {
-	       0: {id: 1, title: "Let's have an event 1!", body: "This is the body for event 1."},
-	       1: {id: 2, title: "Let's have an event 2!", body: "This is the body for event 2."}
-	     }
-	  */
-	  console.log(action.response.data)// 配列
-	  // この方法でデータを扱いたい
-	  console.log(_.mapKeys(action.response.data, 'id')) // keyでobject
-      return state
+	  // keyでデータ扱う
+	  return _.mapKeys(action.response.data, 'id')
 	default:
-	  return state
+	  return evens
   }
 }
